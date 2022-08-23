66075eedbfd765b624ca25d156d09dfe057f3e66
Akhil-2020171 <88500527+Akhil-2020171@users.noreply.github.com>
2022-08-23 15:41:11 +0530

    Update ECE_LAB_Project.ino

--- ECE_LAB_Project.ino
+++ ECE_LAB_Project.ino
@@ -1,52 +1,42 @@
+#include <HTTPSRedirect.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecureBearSSL.h>
 
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 //-----------------------------------------
 constexpr uint8_t RST_PIN = D3;
 constexpr uint8_t SS_PIN = D4;
-//constexpr uint8_t BUZZER = D2;
+constexpr uint8_t BUZZER = D8;
 //-----------------------------------------
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;  
 MFRC522::StatusCode status;      
 //-----------------------------------------
-/* Set the block to write data */
-int blockNum = 2;  
 /* This is the actual data which is going to be written into the card */
-byte blockData [16] = {"ECE"};
+const String blockData = "ECE";
+String card_uid;          
 //-----------------------------------------
-/* Create array to read data from Block */
-/* Length of buffer should be 2 Bytes more than the size of Block */
-byte bufferLen = 18;
-byte readBlockData[18];
-//-----------------------------------------
-//-----------------------------------------
-String card_holder_name;
-String card_uid;
-const String sheet_url = "script url + ?";
-//-----------------------------------------
-const uint8_t fingerprint[20] = {0xc2,0x29,0xb0,0x5e,0xf8,0x50,0x51,0x3c,0x3e,0x18,0x38,0xc4,0x87,0x9f,0xdd,0xde,0x5b,0xc4,0x00,0x77};
-//-----------------------------------------
-//-----------------------------------------
-#define WIFI_SSID "realme"
-#define WIFI_PASSWORD "1234@1234"
-//-----------------------------------------
+#define WIFI_SSID "GUEST-N"
+#define WIFI_PASSWORD "cisco@2022"
+
+// Google Sheets setup (do not edit)
+const char* HOST = "script.google.com";
+const int httpsPort = 443;
+String sheet_url = "/macros/s/AKfycbyWhEUR8LQkKpDRni9nADBQ1doO4R4OakPp4AMnnuIg9N9pOQ_RWDkY1-DWw4dUNJBt/exec";
+HTTPSRedirect* client = nullptr;
+String payload_base =  "{\"values\": ";
+String payload = "";
 
 void setup(){
   //-----------------------------------------
@@ -56,33 +46,90 @@ void setup(){
   //Initialize SPI bus
   SPI.begin();
   //--------------------------------------------------
+  /* Set BUZZER as OUTPUT */
+  pinMode(BUZZER, OUTPUT);
+  //--------------------------------------------------
+  
   //WiFi Connectivity
-  Serial.println();
+  Serial.println("Booting");
   Serial.print("Connecting to Wi-Fi");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(200);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
+  Serial.println('\n');  
+    
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
+  Serial.println("Ready");
   Serial.println("");
   Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.print("Node MCU board MAC-Address : ");
   Serial.println(WiFi.macAddress());
   Serial.println();
   //--------------------------------------------------
+  // Use HTTPSRedirect class to create a new TLS connection
+  client = new HTTPSRedirect(httpsPort);
+  client->setInsecure();
+  client->setPrintResponseBody(true);
+  client->setContentTypeHeader("application/json");
+  // Try to connect for a maximum of 5 times
+  bool flag = false;
+  for (int i=0; i<5; i++){ 
+    int retval = client->connect(HOST, httpsPort);
+    if (retval == 1){
+       flag = true;
+       Serial.println("Connected");
+       break;
+    }
+    else
+      Serial.println("Connection failed. Retrying...");
+  }
+  if (!flag){
+    Serial.print("Could not connect to server: ");
+    Serial.println(HOST);
+    return;
+  }  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(HOST);
+
+  delete client;    // delete HTTPSRedirect object
+  client = nullptr; // delete HTTPSRedirect object
   
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
+  //-------------------------------------------------
+  Serial.println("Scan a MIFARE 1K Tag to Detect data...");
 }
 
-void loop(){ 
-  //Initialize MFRC522 Module
-  mfrc522.PCD_Init();
-  
+void loop(){   
+  ArduinoOTA.handle();
+   
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
@@ -90,15 +137,16 @@ void loop(){
   display.println("Scan");
   display.display();
   
-  setup1();
   setup2();
 }
 
-// Reader Code //
-void setup1(){
+// Writer Code in Google Sheets//
+ void setup2(){
+  //Initialize MFRC522 Module
+  mfrc522.PCD_Init();
   /* Prepare the ksy for authentication */
   /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
-  for (byte i = 0; i < 6; i++){
+  for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
   /* Look for new cards */
@@ -106,9 +154,7 @@ void setup1(){
   if ( ! mfrc522.PICC_IsNewCardPresent()) {return;}
   /* Select one of the cards */
   if ( ! mfrc522.PICC_ReadCardSerial()) {return;}
-
   //------------------------------------------------------------------------------
-  Serial.println("Scan a MIFARE 1K Tag to Detect data...");
   //------------------------------------------------------------------------------
   Serial.print("\n");
   Serial.println("**Card Detected**");
@@ -129,173 +175,60 @@ void setup1(){
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
   Serial.println(mfrc522.PICC_GetTypeName(piccType));
-  //------------------------------------------------------------------------------
-   /* Call 'WriteDataToBlock' function, which will write data to the block */
-   Serial.print("\n");
-   Serial.println("Writing to Data Block...");
-   WriteDataToBlock(blockNum, blockData);
-   //------------------------------------------------------------------------------
-   Serial.print("\n");
-   //------------------------------------------------------------------------------
-  
-  display.clearDisplay();
-  display.setTextSize(1.5);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  display.println("Data has been written inside the Tag.");
-  display.display();
-  delay(500);
-  display.clearDisplay();
-}
-
-// Writer Code in Google Sheets//
- void setup2(){
-  //Initialize MFRC522 Module
-  mfrc522.PCD_Init();
-  /* Look for new cards */
-  /* Reset the loop if no new card is present on RC522 Reader */
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {return;}
-  /* Select one of the cards */
-  if ( ! mfrc522.PICC_ReadCardSerial()) {return;}
-  /* Read data from the same block */
   //--------------------------------------------------
-  Serial.println();
-  Serial.println(F("Reading last data from RFID..."));
-  ReadDataFromBlock(blockNum, readBlockData);
-  /* If you want to print the full memory dump, uncomment the next line */
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-  
-  /* Print the data read from block */
-  Serial.println();
-  Serial.print(F("Last data in RFID:"));
-  Serial.print(blockNum);
-  Serial.print(F(" --> "));
-  
-  for (int j=0 ; j<16 ; j++){
-    Serial.write(readBlockData[j]);
-  }
   Serial.println();  
-  if (WiFi.status() == WL_CONNECTED) {
-    //-------------------------------------------------------------------------------
-    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-    //-------------------------------------------------------------------------------
-    client->setFingerprint(fingerprint);
-    //-----------------------------------------------------------------
-    card_holder_name = sheet_url +"id=" + card_uid +"&name="+String((char*)readBlockData);
-    card_holder_name.trim();
-    Serial.println(card_holder_name);
-    //-----------------------------------------------------------------
-    HTTPClient https;
-    Serial.print(F("[HTTPS] begin...\n"));
-    //-----------------------------------------------------------------
 
-    if (https.begin(*client,card_holder_name)){
-      //-----------------------------------------------------------------
-      // HTTP
-      Serial.print(F("[HTTPS] GET...\n"));
-      // start connection and send HTTP header
-      int httpCode = https.GET();
-      //-----------------------------------------------------------------
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-        // file found at server
-        display.clearDisplay();
-        display.setTextSize(1.5);
-        display.setTextColor(WHITE);
-        display.setCursor(0, 10);
-        display.println("Data has been written in the Google Sheet.");
-        display.display();
-        delay(1000);
-        display.clearDisplay();
-      }
-      //-----------------------------------------------------------------
-      else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-        display.clearDisplay();
-        display.setTextSize(1.5);
-        display.setTextColor(WHITE);
-        display.setCursor(0, 10);
-        display.println("Data hasn't been written in the Google Sheet.");
-        display.display();
-        delay(1000);
-        display.clearDisplay();
-      }
-      //-----------------------------------------------------------------
-      https.end();
-      delay(1000);
-    }
-    else {
-      Serial.printf("[HTTPS} Unable to connect\n");
+  static bool flag = false;
+  if (!flag){
+    client = new HTTPSRedirect(httpsPort);
+    client->setInsecure();
+    flag = true;
+    client->setPrintResponseBody(true);
+    client->setContentTypeHeader("application/json");
+  }
+  if (client != nullptr){
+    if (!client->connected()){
+      client->connect(HOST, httpsPort);
     }
   }
-}
-/****************************************************************************************************
- * ReadDataFromBlock() function
- ****************************************************************************************************/
-void ReadDataFromBlock(int blockNum, byte Data[]) { 
-  //----------------------------------------------------------------------------
-  /* Prepare the ksy for authentication */
-  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
+  else{
+    Serial.println("Error creating client object!");
   }
-  //----------------------------------------------------------------------------
-  /* Authenticating the desired data block for Read access using Key A */
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
-  //----------------------------------------------------------------------------s
-  if (status != MFRC522::STATUS_OK){
-     Serial.print("Authentication failed for Read: ");
-     Serial.println(mfrc522.GetStatusCodeName(status));
-     return;
-  }
-  //----------------------------------------------------------------------------
-  else {
-    Serial.println("Authentication success");
-  }
-  //----------------------------------------------------------------------------
-  /* Reading data from the Block */
-  status = mfrc522.MIFARE_Read(blockNum, Data, &bufferLen);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("Reading failed: ");
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-  //----------------------------------------------------------------------------
-  else {
-    Serial.println("Block was read successfully");  
-  }
-  //----------------------------------------------------------------------------
-}
 
-/****************************************************************************************************
- * Writ() function
- ****************************************************************************************************/
-void WriteDataToBlock(int blockNum, byte Data[]) {
-  //------------------------------------------------------------------------------
-  /* Authenticating the desired data block for write access using Key A */
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK){
-    Serial.print("Authentication failed for Write: ");
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
+  // Create json object string to send to Google Sheets
+  payload = payload_base + "\"" + card_uid + "," + blockData + "\"}";
+  
+  // Publish data to Google Sheets
+  Serial.println("Publishing data...");
+  Serial.println(payload);
+  if(client->POST(sheet_url, HOST, payload)){ 
+    Serial.println("Successful Connection : Data has been transferred to Google Sheets.");
+    display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    display.println("Data has been written in the Google Sheet.");
+    display.display();
+    digitalWrite(BUZZER, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER, LOW);
+    delay(1000);
+    display.clearDisplay();
   }
-  //------------------------------------------------------------------------------
-  else {
-    Serial.println("Authentication success");
+  else{
+    Serial.println("Error while connecting");
+    display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    display.println("Error while connecting");
+    display.display();
+    delay(1000);
+    display.clearDisplay();
   }
-  //------------------------------------------------------------------------------
-  /* Write data to the block */
-  status = mfrc522.MIFARE_Write(blockNum, Data ,16);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("Writing to Block failed: ");
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-  else
-  {Serial.println("Data was written into Block successfully");}
-  //------------------------------------------------------------------------------
+
+  // a delay of several seconds is required before publishing again    
+  delay(1000);
 }
 
 void array_to_string(byte array[], unsigned int len, char buffer[]){
